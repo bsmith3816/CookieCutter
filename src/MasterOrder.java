91f338411e7d2515c9872e6975f43c4093be3a62
BT_1N3_27 <soconnor5921@bths.edu>
2018-12-17 10:14:01 -0500

    fwe';feknw ]gmgeklgken;bn

--- src/MasterOrder.java
+++ src/MasterOrder.java
@@ -1,2 +1,41 @@
-public class MasterOrder {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MasterOrder
+{
+    private List<CookieOrder> orders;
+
+    public MasterOrder()
+    {
+        orders = new ArrayList<CookieOrder>();
+    }
+
+    public void addOrder(CookieOrder theOrder)
+    {
+        orders.add(theOrder);
+    }
+
+    public int getTotalBoxes()
+    {
+        int sum = 0;
+        for(int i = 0; i < orders.size(); i++){
+            sum += orders.get(i).getNumBoxes();
+        }
+        return sum;
+    }
+
+    public int removeVariety(String cookieVar)
+    {
+        int sum = 0;
+        for(int i = 0; i < orders.size(); i++)
+        {
+            if(cookieVar.equals(orders.get(i).getVariety()))
+            {
+                sum += orders.get(i).getNumBoxes();
+                orders.remove(i);
+                i--;
+            }
+        }
+        return sum;
+    }
 }
