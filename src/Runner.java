2aa765b416db07f3c3babdf08e7ee12d5c5045f0
BT_1N3_28 <@>
2018-12-17 10:14:49 -0500

    12.17 #3

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+
 public class Runner {
+    public static void main (String[] args){
+        MasterOrder goodies = new MasterOrder();
+        goodies.addOrder(new CookieOrder("Chocolate Chip", 1));
+        goodies.addOrder(new CookieOrder("Shortbread", 5));
+        goodies.addOrder(new CookieOrder("Macaroon", 2));
+        goodies.addOrder(new CookieOrder("Chocolate Chip", 3));
+        System.out.println(Arrays.toString(goodies));
+        System.out.println(goodies.removeVariety("Chocolate Chip"));
+        System.out.println(Arrays.toString(goodies));
+    }
 }
